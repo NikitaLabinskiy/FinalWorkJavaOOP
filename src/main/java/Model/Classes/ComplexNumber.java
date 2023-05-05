cea07605e93c0e65c7e3aba4693d1bd196807404
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-05-05 11:13:39 +0300

    Add class ComplexNumber

--- src/main/java/Model/Classes/ComplexNumber.java
+++ src/main/java/Model/Classes/ComplexNumber.java
@@ -1,42 +1,45 @@
-package Model;
+package Model.Classes;
 
 import lombok.Data;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 @Data
 public class ComplexNumber {
-    private int real;
-    private int imaginary;
+    private Double real;
+    private Double imaginary;
 
-    public ComplexNumber(int real, int imaginary) {
+    DecimalFormat format = new DecimalFormat();
+
+    public ComplexNumber(Double real, Double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
 
     public ComplexNumber(Scanner scanner) {
         System.out.print("\nВведите вещественную часть: ");
-        this.real = scanner.nextInt();
+        this.real = scanner.nextDouble();
         System.out.print("Введите мнимую часть: ");
-        this.imaginary = scanner.nextInt();
+        this.imaginary = scanner.nextDouble();
         if (imaginary == 0)
-            System.out.println("\nСформированное число = " + getReal());
+            System.out.println("\nСформированное число = " + format.format(getReal()));
         if (imaginary < 0)
-            System.out.println("\nСформированное число = " + getReal() + "-" + getImaginary() + "i");
+            System.out.println("\nСформированное число = " + format.format(getReal()) + format.format(getImaginary()) + "i");
         if (imaginary > 0)
-            System.out.println("\nСформированное число = " + getReal() + "+" + getImaginary() + "i");
+            System.out.println("\nСформированное число = " + format.format(getReal()) + "+" + format.format(getImaginary()) + "i");
     }
 
-    public ComplexNumber(int real) {
+    public ComplexNumber(Double real) {
         this.real = real;
-        this.imaginary = 0;
+        this.imaginary = 0.0;
     }
 
     @Override
     public String toString() {
-        if (imaginary == 0) System.out.println(getReal());
-        if (imaginary < 0) System.out.println(getReal() + "-" + getImaginary() + "i");
-        if (imaginary > 0) System.out.println(getReal() + "+" + getImaginary() + "i");
+        if (imaginary == 0) System.out.println(format.format(getReal()));
+        if (imaginary < 0) System.out.println(format.format(getReal()) + format.format(getImaginary()) + "i");
+        if (imaginary > 0) System.out.println(format.format(getReal()) + "+" + format.format(getImaginary()) + "i");
         return null;
     }
 }
