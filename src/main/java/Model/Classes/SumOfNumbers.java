35d7baa07e441c3d50acb5f2ea577b0afdb7c8cd
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-05-05 11:15:59 +0300

    Add class SumOfNumbers

--- src/main/java/Model/Classes/SumOfNumbers.java
+++ src/main/java/Model/Classes/SumOfNumbers.java
@@ -1,2 +1,17 @@
-package Model.Classes;public class SumOfNumbers {
+package Model.Classes;
+
+import Model.Interfaces.ArithmeticOperation;
+
+public class SumOfNumbers implements ArithmeticOperation {
+    @Override
+    public ComplexNumber arithmeticOperation(ComplexNumber first, ComplexNumber second) {
+        double real = first.getReal() + second.getReal();
+        double imaginary = first.getImaginary() + second.getImaginary();
+        ComplexNumber complexNumber;
+        if (imaginary == 0)
+            complexNumber = new ComplexNumber(real);
+        else
+            complexNumber = new ComplexNumber(real, imaginary);
+        return complexNumber;
+    }
 }
