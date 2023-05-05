d0dd730b0a3b0ccabb7585bb1db063f0b03445f6
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-05-05 11:16:48 +0300

    Add interface ArithmeticOperation

---
+++ src/main/java/Model/Interfaces/ArithmeticOperation.java
@@ -0,0 +1,7 @@
+package Model.Interfaces;
+
+import Model.Classes.ComplexNumber;
+
+public interface ArithmeticOperation {
+    public ComplexNumber arithmeticOperation(ComplexNumber first, ComplexNumber second);
+}
