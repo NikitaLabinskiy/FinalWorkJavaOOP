e4f02a591cab9939296ebd5abadac8d1890ccc65
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-05-05 11:10:22 +0300

    Add class Main

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Controller.Controller;
+import Model.Classes.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new Controller();
+    }
+}
